cf85f0eba80a30bd6098d29530c4b918a36ecf41
Ubuntu <ubuntu@nginx.f5lab.com>
2023-08-14 18:57:02 +0000

    my first commit

---
+++ njs/KvOperations.js
@@ -0,0 +1,32 @@
+async function CheckKv (r) {
+  if (r.variables.usertoken) {
+    //
+    //put user info validation code here
+    //
+    //if (r.variables.http_remote_user.length < 2) {
+    //  r.return(403);
+    //  return;
+   // }
+    let newtoken = await r.subrequest("/auth"); 
+    r.variables.usertoken = newtoken.responseBody;
+    r.return(204);
+    return;
+  }
+  else if (!r.variables.usertoken) {
+    //
+    //dummy key validation code
+    //
+    if (r.variables.usertoken.length < 10) {
+      r.return(403);
+      return;
+    }
+    r.return(204);
+    return;
+  }
+  else {
+    r.return(403);
+    return;
+  }
+}
+
+export default { CheckKv };
